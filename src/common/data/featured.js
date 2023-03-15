0fa9b94454a4f8cc697ba28af02941fd03ccd55e
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-03-15 13:37:28 +0100

    feat: update featured dapps

--- src/common/data/featured.js
+++ src/common/data/featured.js
@@ -1,34 +1,35 @@
 import inchBanner from '../assets/images/featured/inch_banner.png'
 import inchLogo from '../assets/images/featured/inch_logo.png'
-import GitcoinBanner from '../assets/images/featured/gitcoin_banner.png'
-import GitcoinLogo from '../assets/images/featured/gitcoin_logo.png'
-import MatchaBanner from '../assets/images/featured/matcha_banner.png'
-import MatchaLogo from '../assets/images/featured/matcha_logo.png'
+import HopersBanner from '../assets/images/featured/hopers.png'
+import HopersLogo from '../assets/images/featured/hopers_logo.png'
+import KujiraFinBanner from '../assets/images/featured/kujira_fin.png'
+import KujiraFinLogo from '../assets/images/featured/kujira_fin_logo.png'
+import OsmosisBanner from '../assets/images/featured/osmosis.png'
+import OsmosisLogo from '../assets/images/featured/osmosis_logo.png'
 
 const featuredDapps = [
   {
-    name: '1inch.exchange',
-    description:
-      'The most efficient DeFi / DEX aggregator with the best exchange rates accross Ethereum and Binance Smart Chain.',
-    url: 'https://1inch.exchange/',
-    banner: inchBanner,
-    icon: inchLogo,
+    name: 'Osmosis',
+    description: 'Osmosis is the premier cross-chain DeFi hub.',
+    url: 'https://frontier.osmosis.zone/',
+    banner: OsmosisBanner,
+    icon: OsmosisLogo,
   },
   {
-    name: 'Matcha',
+    name: 'Hopers',
     description:
-      'Your new favorite DEX aggregator. Find the best prices across exchange networks. Powered by 0x Labs.',
-    url: 'https://matcha.xyz/?utm_source=statusnetwork&utm_medium=dappportal',
-    banner: MatchaBanner,
-    icon: MatchaLogo,
+      'The first fully-permissionless DEX living in the Cosmos. Friction-less yield.',
+    url: 'https://hopers.io',
+    banner: HopersBanner,
+    icon: HopersLogo,
   },
   {
-    name: 'Gitcoin Grants',
+    name: 'Kujira Fin',
     description:
-      'Gitcoin Grants helps creators grow and sustain their open source projects.',
-    url: 'https://gitcoin.co/grants',
-    banner: GitcoinBanner,
-    icon: GitcoinLogo,
+      "Cosmos's first decentralized, permissionless, 100% on-chain, order book style token exchange.",
+    url: 'https://fin.kujira.app',
+    banner: KujiraFinBanner,
+    icon: KujiraFinLogo,
   },
 ]
 
