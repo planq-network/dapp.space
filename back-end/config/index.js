1f886ca1cb40acd3fb8e3202b8f5612e9154144e
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-03-16 18:50:20 +0100

    add IPFS api env

--- back-end/config/index.js
+++ back-end/config/index.js
@@ -10,15 +10,16 @@ const config = {
   RATE_LIMIT_TIME              : env.RATE_LIMIT_TIME              || 15,
   RATE_LIMIT_MAX_REQ           : env.RATE_LIMIT_MAX_REQ           || 1,
   /* Misc */
-  ENVIRONMENT                  : env.ENVIRONMENT                  || "PROD",
+  ENVIRONMENT                  : env.ENVIRONMENT                  || "DEV",
   /* Database */
   DB_CONNECTION                : env.DB_CONNECTION                || null,
   /* Access */
   ADMIN_USER                   : env.ADMIN_USER                   || "admin",
   ADMIN_PASSWORD               : env.ADMIN_PASSWORD               || "discoverbancor",
   /* IPFS */
-  IPFS_HOST                    : env.IPFS_HOST                    || "ipfs.planq.network",
-  IPFS_PORT                    : env.IPFS_PORT                    || "443",
+  IPFS_HOST                    : env.IPFS_HOST                    || "ipfs.infura.io",
+  IPFS_API                     : env.IPFS_API                     || null,
+  IPFS_PORT                    : env.IPFS_PORT                    || "5001",
   IPFS_PROTOCOL                : env.IPFS_PROTOCOL                || "https",
   /* Blockchain */
   DISCOVER_CONTRACT            : env.DISCOVER_CONTRACT            || "0xAE4Bb282F92349D00A3109E485FafC745Dd945C4",
@@ -29,8 +30,8 @@ const config = {
   EMAIL_HOST                   : env.EMAIL_HOST                   || null,
   EMAIL_PORT                   : env.EMAIL_PORT                   || null,
   EMAIL_TLS                    : env.EMAIL_TLS                    || null,
-  APPROVER_MAIL                : env.APPROVER_MAIL                || "dapps-approvals@status.im",
-  APPROVE_NOTIFIER_MAIL        : env.APPROVE_NOTIFIER_MAIL        || "dapps-approvals@status.im",
+  APPROVER_MAIL                : env.APPROVER_MAIL                || "dapp.space@planq.network",
+  APPROVE_NOTIFIER_MAIL        : env.APPROVE_NOTIFIER_MAIL        || "dapp.space@planq.network",
   /* Logging */
   CLOUDWATCH_ACCESS_KEY_ID     : env.CLOUDWATCH_ACCESS_KEY_ID     || null,
   CLOUDWATCH_SECRET_ACCESS_KEY : env.CLOUDWATCH_SECRET_ACCESS_KEY || null,
