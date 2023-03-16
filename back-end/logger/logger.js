d09039d835184ba5f6de466c47661a432c7c0ef7
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-03-16 18:50:30 +0100

    add extra logging levels

--- back-end/logger/logger.js
+++ back-end/logger/logger.js
@@ -17,9 +17,14 @@ let cloudWatchLogConfig = Object.assign({ logStreamName: `application-logs` }, b
 
 let loggerLevels = {
     levels: {
-        error: 1,
-        warn: 2,
-        info: 3
+      emerg: 0,
+      alert: 1,
+      crit: 2,
+      error: 3,
+      warning: 4,
+      notice: 5,
+      info: 6,
+      debug: 7
     }
 }
 
