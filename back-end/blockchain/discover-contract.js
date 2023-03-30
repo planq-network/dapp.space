b65c88469b6a492e4274697de880730e8730868e
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-03-30 18:12:16 +0200

    cleanup

--- back-end/blockchain/discover-contract.js
+++ back-end/blockchain/discover-contract.js
@@ -1,11 +1,9 @@
 const web3 = require('ethers')
 const config = require('./../config')
 const DiscoverABI = require('./discover-abi.json')
-const Web3 = require("ethers");
 
 module.exports = new web3.Contract(
   config.DISCOVER_CONTRACT,
   DiscoverABI,
-  new Web3.providers.JsonRpcProvider(config.BLOCKCHAIN_CONNECTION_POINT),
-  7070,
+  new web3.providers.JsonRpcProvider(config.BLOCKCHAIN_CONNECTION_POINT, 7070),
 )
