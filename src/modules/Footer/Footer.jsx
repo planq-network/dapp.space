c6a16296241c1489f7f4602498a38c9412134455
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-04-05 14:26:55 +0200

    open discord in new window

--- src/modules/Footer/Footer.jsx
+++ src/modules/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = props => {
 
   return (
     <div className={styles.footer}>
-      <a href="https://discord.gg/planq-network" className={styles.footerItem}>
+      <a href="https://discord.gg/planq-network" target="_blank" className={styles.footerItem}>
         <div className={styles.iconWrap}>
           <img src={communityIcon} alt="Join the Planq community chat" />
         </div>
