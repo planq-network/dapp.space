25c0d03dfb2ac993b7977528e3e836de5757fb79
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2023-06-02 16:32:34 +0200

    update featured dapps

--- src/common/data/featured.js
+++ src/common/data/featured.js
@@ -1,5 +1,5 @@
-import OsmosisBanner from '../assets/images/featured/osmosis.png'
-import OsmosisLogo from '../assets/images/featured/osmosis_logo.png'
+import UnUniFiBanner from '../assets/images/featured/ununifi_banner.jpg'
+import UnUniFiLogo from '../assets/images/featured/ununifi_logo.png'
 import YieldmosBanner from '../assets/images/featured/yieldmos.png'
 import YieldmosLogo from '../assets/images/featured/yieldmos_logo.png'
 import NrideBanner from '../assets/images/featured/nride.png'
@@ -15,20 +15,20 @@ const featuredDapps = [
     icon: NrideLogo,
   },
   {
+    name: 'UnUniFi',
+    description:
+      'UnUniFi is a fast, open, interoperable smart contracts layer 1 blockchain optimized for NFT-Fi infrastructure.',
+    url: 'https://lab-testnet.ununifi.io/',
+    banner: UnUniFiBanner,
+    icon: UnUniFiLogo,
+  },
+  {
     name: 'Yieldmos',
     description:
       'Non-custodial yield optimization and asset management for the Cosmos-verse | Not Smart Contract Based',
     url: 'https://www.yieldmos.com/strategies',
     banner: YieldmosBanner,
     icon: YieldmosLogo,
-  },
-  {
-    name: 'Osmosis',
-    description:
-      'Osmosis is the premier cross-chain DeFi hub.',
-    url: 'https://app.osmosis.zone/',
-    banner: OsmosisBanner,
-    icon: OsmosisLogo,
   },
 ]
 
